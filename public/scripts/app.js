6644bd53fb3c1a5f7a6d2bdeb7d6a65576565310
Sikander Soomro <49952081+soomrous@users.noreply.github.com>
2023-03-04 01:51:15 -0500

    updated appjs file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,13 +2,78 @@
 
 console.log('App.js is running');
 
-//JSX - JavaScript XML 
+var app = {
+    title: 'Indecision App!!',
+    subtitle: 'Put your life in the hands of a computer',
+    option: ['one', 'two']
+};
 
 var template = React.createElement(
-  'h1',
-  null,
-  ' Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.option.length > 0 ? 'Here are your options' : 'No Option'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
 );
+var user = {
+    name: 'Sikander Ali Soomro',
+    age: "42",
+    location: 'Winston Salem NC 27106'
+
+};
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            ' Location: ',
+            location
+        );
+    }
+}
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        ' Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
 console.log('App.js is running');
 
-//JSX - JavaScript XML 
+var app = {
+    title: 'Indecision App!!',
+    subtitle: 'Put your life in the hands of a computer',
+    option: ['one', 'two']
+}
 
-var template = <h1> Indecision App</h1>;
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.option.length > 0 ? 'Here are your options':'No Option'}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
+var user = {
+    name: 'Sikander Ali Soomro',
+    age: "42",
+    location: 'Winston Salem NC 27106'
+
+};
+function getLocation(location){
+    if (location) {
+        return <p> Location: {location}</p>;
+    }
+}
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p> Age: {user.age}</p>}
+        {getLocation(user.location)}
+
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
